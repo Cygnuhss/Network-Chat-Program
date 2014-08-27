9d6951e6ead128d9e479787723aa35b3e4b55c0d
Cygnuhss <jelmervannuss@yahoo.com>
2014-08-27 13:52:36 +0200

    Episode 2: The Login Window

--- src/com/NetworkChatProgram/Login.java
+++ src/com/NetworkChatProgram/Login.java
@@ -1,24 +1,38 @@
 package com.NetworkChatProgram;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class Login extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private JPanel contentPane;
+	private JTextField txtName;
 
 	public Login() {
+		setResizable(false);
+		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setSize(300, 380);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtName = new JTextField();
+		txtName.setBounds(64, 60, 165, 20);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Name:");
+		lblNewLabel.setBounds(124, 43, 46, 14);
+		contentPane.add(lblNewLabel);
 	}
 	
 	public static void main(String[] args) {
@@ -33,5 +47,4 @@ public class Login extends JFrame {
 			}
 		});
 	}
-	
 }
