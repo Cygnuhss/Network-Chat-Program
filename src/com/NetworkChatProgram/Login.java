e9982014f086ee0317df73fdafb9a7f56ad6562e
Cygnuhss <jelmervannuss@yahoo.com>
2014-08-30 12:49:50 +0200

    Episode 3: Login Form GUI

--- src/com/NetworkChatProgram/Login.java
+++ src/com/NetworkChatProgram/Login.java
@@ -7,14 +7,27 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private JPanel contentPane;
 	private JTextField txtName;
+	private JTextField txtAddress;
+	private JTextField txtPort;
 
 	public Login() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		setResizable(false);
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,9 +43,43 @@ public class Login extends JFrame {
 		contentPane.add(txtName);
 		txtName.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Name:");
-		lblNewLabel.setBounds(124, 43, 46, 14);
-		contentPane.add(lblNewLabel);
+		JLabel lblName = new JLabel("Name:");
+		lblName.setBounds(131, 43, 31, 14);
+		contentPane.add(lblName);
+		
+		txtAddress = new JTextField();
+		txtAddress.setBounds(64, 127, 165, 20);
+		contentPane.add(txtAddress);
+		txtAddress.setColumns(10);
+		
+		JLabel lblAddress = new JLabel("IP Address:");
+		lblAddress.setBounds(118, 110, 57, 14);
+		contentPane.add(lblAddress);
+		
+		txtPort = new JTextField();
+		txtPort.setColumns(10);
+		txtPort.setBounds(64, 190, 165, 20);
+		contentPane.add(txtPort);
+		
+		JLabel lblPort = new JLabel("Port:");
+		lblPort.setBounds(135, 173, 24, 14);
+		contentPane.add(lblPort);
+		
+		JLabel lblAddressDesc = new JLabel("(eg. 192.168.0.2)");
+		lblAddressDesc.setBounds(104, 148, 87, 14);
+		contentPane.add(lblAddressDesc);
+		
+		JLabel lblPortDesc = new JLabel("(eg. 8192)");
+		lblPortDesc.setBounds(121, 212, 51, 14);
+		contentPane.add(lblPortDesc);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnLogin.setBounds(104, 277, 87, 20);
+		contentPane.add(btnLogin);
 	}
 	
 	public static void main(String[] args) {
