0ce53c998082aab25bce3e81f6cd40f662859f04
Cygnuhss <jelmervannuss@yahoo.com>
2014-08-31 13:38:15 +0200

    Episode 14: Server Command Line Arguments

---
+++ src/com/NetworkChatProgram/server/ServerMain.java
@@ -0,0 +1,21 @@
+package com.NetworkChatProgram.server;
+
+public class ServerMain {
+	private int port;
+	
+	public ServerMain(int port) {
+		this.port = port;
+		System.out.println(port);
+	}
+	
+    public static void main(String[] args) {
+    	int port;
+    	if (args.length != 1) {
+    		System.out.println("Usage: java -jar NetworkChatProgram.jar [port]");
+    	    return;
+    	}
+    	port = Integer.parseInt(args[0]);
+    	new ServerMain(port);
+    }
+    
+}
