48d517bc7bdab27b3eaf0b39634fb19ab5c8a61e
Cygnuhss <jelmervannuss@yahoo.com>
2014-08-31 16:19:07 +0200

    Episode 15: Server Threads and Explanations

--- src/com/NetworkChatProgram/server/Server.java
+++ src/com/NetworkChatProgram/server/Server.java
@@ -1,10 +1,53 @@
 package com.NetworkChatProgram.server;
 
-public class Server {
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Server implements Runnable {
+	private DatagramSocket socket;
     private int port;
+    
+    private boolean running = false;
+    private Thread run;
+    private Thread manage;
+    private Thread send;
+    private Thread receive;
     
     public Server(int port) {
     	this.port = port;
+    	try {
+			socket = new DatagramSocket(port);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+    	run = new Thread(this, "Server");
     }
+
+	public void run() {
+		running = true;
+		manageClients();
+		receive();
+	}
     
+	private void manageClients() {
+		manage = new Thread(this, "Manage") {
+			public void run() {
+				while (running) {
+					// Managing.
+				}
+			}
+		};
+		manage.start();
+	}
+	
+	private void receive() {
+		receive = new Thread(this, "Receive") {
+			public void run() {
+				while (running) {
+					// Receiving.
+				}
+			}
+		};
+		receive.start();
+	}
 }
--- src/com/NetworkChatProgram/server/ServerMain.java
+++ src/com/NetworkChatProgram/server/ServerMain.java
@@ -2,10 +2,11 @@ package com.NetworkChatProgram.server;
 
 public class ServerMain {
 	private int port;
+	private Server server;
 	
 	public ServerMain(int port) {
 		this.port = port;
-		System.out.println(port);
+		server = new Server(port);
 	}
 	
     public static void main(String[] args) {
